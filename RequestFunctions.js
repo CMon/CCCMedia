d108ad218f35b396e122f84e931689db5b8405bf
Simon Schäfer <simon.schaefer@koeln.de>
2015-01-11 23:58:50 +0100

    added javascript to load a url

--- RequestFunctions.js
+++ RequestFunctions.js
@@ -1,6 +1,20 @@
 .pragma library
 
-function func() {
+function getList(url, callback) {
+    console.log("updating " + url)
+    var xhr = new XMLHttpRequest();
 
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == xhr.DONE) {
+            if (xhr.status == 200) {
+                callback(JSON.parse(xhr.responseText))
+            } else if (xhr.status == 0) {
+                console.log("Error: ", xhr.status, xhr.statusText);
+            }
+        } else if (xhr.status != 200) {
+            console.log("Error: ", xhr.status)
+        }
+    }
+    xhr.send();
 }
-
